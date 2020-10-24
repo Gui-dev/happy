945275d6009d1e33e7881428a10185ae920085be
Gui-dev <adrianon10@gmail.com>
2020-10-24 03:01:03 -0300

    refactor: add comoponent GlobalStyle

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import GlobalStyle from './assets/style/GlobalStyle'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </>
   )
 }
 
