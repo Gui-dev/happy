5d363c3c4bb9d0a28eccd3697ed858f1323bc2d5
Gui-dev <adrianon10@gmail.com>
2020-10-28 03:15:30 -0300

    feat: add app config

---
+++ backend/src/App.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+
+export class App {
+
+  public app: express.Application
+
+  constructor () {
+    this.app = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares () {
+    this.app.use(express.json())
+    this.app.use(cors())
+  }
+
+  routes () {
+
+  }
+} 
