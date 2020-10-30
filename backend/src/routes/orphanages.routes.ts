1cb88588eec2cc09abfe22afdc5c8985f3abe4ec
Gui-dev <adrianon10@gmail.com>
2020-10-30 02:56:05 -0300

    feat: create route get to list an orphanages

--- backend/src/routes/orphanages.routes.ts
+++ backend/src/routes/orphanages.routes.ts
@@ -6,6 +6,7 @@ const orphanagesRoutes = Router()
 const orphanagesController = new OrphanagesController()
 
 orphanagesRoutes.get('/', orphanagesController.index)
+orphanagesRoutes.get('/:id', orphanagesController.show)
 orphanagesRoutes.post('/', orphanagesController.create)
 
 export default orphanagesRoutes
