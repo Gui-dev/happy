f8f190344baa92f65fac1ff99efb41bea3799962
Gui-dev <adrianon10@gmail.com>
2020-10-30 19:17:45 -0300

    feat: add multer to images uploads

--- backend/src/routes/orphanages.routes.ts
+++ backend/src/routes/orphanages.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { OrphanagesController } from './../controllers/OrphanagesController'
+import multerConfig from './../config/upload'
 
 const orphanagesRoutes = Router()
 const orphanagesController = new OrphanagesController()
+const upload = multer(multerConfig)
 
 orphanagesRoutes.get('/', orphanagesController.index)
 orphanagesRoutes.get('/:id', orphanagesController.show)
-orphanagesRoutes.post('/', orphanagesController.create)
+orphanagesRoutes.post('/', upload.array('images'), orphanagesController.create)
 
 export default orphanagesRoutes
