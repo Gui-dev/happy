b742b01c609727c11f8bf4dd2b83c6943e972718
Gui-dev <adrianon10@gmail.com>
2020-10-31 02:54:17 -0300

    feat: create view to render props from Images

---
+++ backend/src/views/ImagesView.ts
@@ -0,0 +1,15 @@
+import { Image } from './../models/Image'
+
+export class ImagesView {
+
+  public render (image: Image) {
+    return {
+      id: image.id,
+      url: `http://192.168.0.106:3333/uploads/${image.path}`
+    }
+  }
+
+  public renderMany (images: Image[]) {
+    return images.map(image => this.render(image))
+  }
+}
