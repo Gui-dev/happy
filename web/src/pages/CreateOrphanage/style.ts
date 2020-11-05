1276749d7c988cf721bce38314e0ebfd2b9ff2e2
Gui-dev <adrianon10@gmail.com>
2020-11-05 02:37:27 -0300

    style: remove style Aside

--- web/src/pages/CreateOrphanage/style.ts
+++ web/src/pages/CreateOrphanage/style.ts
@@ -4,39 +4,6 @@ export const Container = styled.div`
   display: flex;
 `
 
-export const Aside = styled.aside`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  position: fixed;
-  height: 100%;
-  padding: 3.2rem 2.4rem;
-  background: linear-gradient(329.54deg, #15B6D6 0%, #15D6D6 100%);
-
-
-  img {
-    width: 4.8rem;
-  }
-
-  a, button {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 4.8rem;
-    height: 4.8rem;
-    background: #12AFCB;
-    border: 0;
-    border-radius: 1.6rem;
-    transition: background-color 0.2s;
-    cursor: pointer;
-
-    &:hover {
-      background: #17D6EB;
-    }
-}
-`
-
 export const Main = styled.main`
   flex: 1;
 `
