e782616cc6b211a7d7081d0ceb23fbcff02f5783
Gui-dev <adrianon10@gmail.com>
2020-11-07 02:45:39 -0300

    feat: add helper method handleMapMarker and add form props states

--- web/src/pages/CreateOrphanage/index.tsx
+++ web/src/pages/CreateOrphanage/index.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import { Map, Marker, TileLayer } from 'react-leaflet'
+import { LeafletMouseEvent } from 'leaflet'
 import { FiPlus } from "react-icons/fi"
 
 import { mapIcon } from './../../utils/mapIcon'
@@ -7,6 +8,18 @@ import { Sidebar } from './../../components/Sidebar'
 import { Container, Main, Form, InputBlock, ButtonSubmit } from './style'
 
 export const CreateOrphanage: React.FC = () => {
+
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
+  const [name, setName] = useState('')
+  const [about, setAbout] = useState('')
+  const [instructions, setInstructions] = useState('')
+  const [opening_hours, setOpeningHours] = useState('')
+  const [open_on_weekend, setOpenOnWeekend] = useState('')
+
+  const handleMapMarker = (event: LeafletMouseEvent) => {
+    const { lat, lng } = event.latlng
+    setPosition({ latitude: lat, longitude: lng })
+  }
 
   return (
     <Container>
@@ -22,22 +35,41 @@ export const CreateOrphanage: React.FC = () => {
               center={[-27.2092052,-49.6401092]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onClick={ handleMapMarker }
             >
               <TileLayer 
                 url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
 
-              <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
+              { position.latitude !== 0 && (
+                <Marker 
+                  interactive={false} 
+                  style={{ width: '100%', height: 280 }}
+                  icon={mapIcon} 
+                  position={[position.latitude, position.longitude]} 
+                />
+              ) }
+
+              
             </Map>
 
             <InputBlock>
               <label htmlFor="name">Nome</label>
-              <input id="name" />
+              <input 
+                id="name"
+                value={ name }
+                onChange={ event => setName(event.target.value) }
+              />
             </InputBlock>
 
             <InputBlock>
               <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
+              <textarea 
+                id="about" 
+                maxLength={300}
+                value={ about }
+                onChange={ event => setAbout(event.target.value) }
+              />
             </InputBlock>
 
             <InputBlock>
@@ -58,12 +90,20 @@ export const CreateOrphanage: React.FC = () => {
 
             <InputBlock>
               <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+              <textarea 
+                id="instructions" 
+                value={ instructions }
+                onChange={ event => setInstructions(event.target.value) }
+              />
             </InputBlock>
 
             <InputBlock>
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+              <label htmlFor="opening_hours">Horário de funcionamento</label>
+              <input 
+                id="opening_hours" 
+                value={ opening_hours }
+                onChange={ event => setOpeningHours(event.target.value) }              
+              />
             </InputBlock>
 
             <InputBlock>
