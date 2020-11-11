4807494cdd66dc5505b4e966429f624da1892a28
Gui-dev <adrianon10@gmail.com>
2020-11-11 03:18:25 -0300

    feat: create page Home

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons'
+
+import mapMarker from './../../assets/images/map-marker.png'
+import { Container, MapContainer, CalloutContainer, CalloutText, Footer,
+  FooterText, CreateOrphanageButton, CreateOrphanageButtonText
+} from './style'
+
+export const Home: React.FC = () => {
+  return (
+    <Container>
+      <MapContainer 
+        provider={ PROVIDER_GOOGLE }
+        initialRegion={{
+          latitude: -23.7833347,
+          longitude: -46.6802013,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+      >
+        <Marker 
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.8,
+            y: 0.9
+          }}
+          coordinate={{
+            latitude: -23.7833347,
+            longitude: -46.6802013,
+          }}
+        >
+          <Callout
+            tooltip
+            onPress={ () => {} }
+          >
+            <CalloutContainer>
+              <CalloutText>Lar de Maria</CalloutText>
+            </CalloutContainer>
+          </Callout>
+        </Marker>
+      </MapContainer>
+
+      <Footer>
+        <FooterText>2 orfanatos encontrados</FooterText>
+
+        <CreateOrphanageButton>
+          <CreateOrphanageButtonText>
+            <Feather name="plus" size={ 20 } color="#FFF"/>
+          </CreateOrphanageButtonText>
+        </CreateOrphanageButton>
+      </Footer>
+    </Container>
+  )
+}
