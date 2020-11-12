3d67fb0792490dc59c284e502f0bd7ec53493c41
Gui-dev <adrianon10@gmail.com>
2020-11-12 02:54:43 -0300

    feat: create route component

---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { OrphanagesMap } from './../screens/OrphanagesMap'
+import { OrphanageDetails } from './../screens/OrphanageDetails'
+
+const Routes: React.FC = () => {
+
+  const { Navigator, Screen } = createStackNavigator()
+
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Screen name="OrphanagesMap" component={ OrphanagesMap }/>
+        <Screen name="OrphanageDetails" component={ OrphanageDetails }/>
+      </Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Routes
